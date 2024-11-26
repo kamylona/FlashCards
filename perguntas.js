020c789ada6ff7020737acbdb0f5dca4d6cfa78f
Kamys <140816456+kamylona@users.noreply.github.com>
2024-11-26 11:23:53 +0000

    a

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,33 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Língua Portuguesa',
+    'O que é um substantivo?',
+    'Substantivo é uma palavra que nomeia as pessoas, lugares, coisas ou ideias.'
+)
+
+criaCartao(
+    'Inglês',
+    'O que significa a frase: "I like potato"?',
+    'Eu gosto de batata.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'O que é um bioma?',
+    'Bioma é uma grande comunidade ecológica caracterizada por condições climáticas específicas, tipos de vegetação e fauna predominante.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'História',
+    'Em que ano ocorreu a Proclamação da República no Brasil?',
+    '1889.'
 )
-
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Sociologia',
+    'Qual o conceito de cultura de acordo com a sociologia?',
+    'Cultura são crenças, costumes e práticas compartilhadas por um grupo humano.'
+)
+criaCartao(
+    'Sociologia',
+    'Qual o conceito de cultura de acordo com a sociologia?',
+    'Cultura são crenças, costumes e práticas compartilhadas por um grupo humano.'
 )
